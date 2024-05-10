f762d99c9e7baadb088302596dd32d7117b67993
ImamTry257 <imamtrywibowo@gmail.com>
2024-05-10 13:31:50 +0700

    fixing register student

--- app/Http/Controllers/front/RegisterController.php
+++ app/Http/Controllers/front/RegisterController.php
@@ -17,35 +17,26 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
+
         if ($request->method() == 'POST') :
             //validate form
             $this->validate($request, [
                 'username'              => 'required|unique:users,name',
                 'password'              => 'required|confirmed',
                 'password_confirmation' => 'required',
-                'school'                => 'required'
+                // 'school'                => 'required'
             ]);
-
-            // check is_email
-            $explode_data = explode('@', $request->username);
-            $username = $request->username;
-            $email = Str::random(10) . '@example.com';
-
-            if (count($explode_data) > 1) :
-                $username = '-';
-                $email = $request->username;
-            endif;
 
             try {
                 DB::table('users')
                     ->insert([
-                        'name' => $username,
-                        'email' => $email,
+                        'name' => $request->username,
+                        'email' => $request->email,
                         'password' => Hash::make($request->password),
-                        'school' => $request->school,
-                        'type'  => 'siswa',
-                        'status' => 1,
-                        'created_by' => 0
+                        // 'school' => $request->school,
+                        // 'type'  => 'siswa',
+                        // 'status' => 1,
+                        // 'created_by' => 0
                     ]);
 
                 return redirect()->route('register.success');
