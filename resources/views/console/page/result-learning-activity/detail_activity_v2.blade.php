7902cfe08a390c84dfc65d8ba60c63a78e26218e
ImamTry257 <imamtrywibowo@gmail.com>
2024-06-02 22:20:17 +0700

    debug data detail activity

--- resources/views/console/page/result-learning-activity/detail_activity_v2.blade.php
+++ resources/views/console/page/result-learning-activity/detail_activity_v2.blade.php
@@ -199,6 +199,9 @@
         </div>
     </div>
 </div>
+@php
+    dump($step_progress);
+@endphp
 <script>
     @foreach ($step_progress as $progress)
         var step_element = $('div#step-' + '{{ $progress->step_id }}')
