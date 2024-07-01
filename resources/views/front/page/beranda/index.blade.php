0e193f7e606e481b144d7944c7614497d3a96b48
imamtry <imemuhiu@gmail.com>
2024-07-01 10:17:03 +0700

    update link youtube

--- resources/views/front/page/beranda/index.blade.php
+++ resources/views/front/page/beranda/index.blade.php
@@ -76,7 +76,7 @@
             <span class="modal-title text-white" id="staticBackdropLabel">Video Panduan</span>
         </div>
         <div class="modal-body text-center">
-            <iframe width="750" height="400" src="https://www.youtube.com/embed/8mK7QrNVenc?si=lqZmhwdvG9jvWux5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="750" height="400" src="https://www.youtube.com/embed/rZ3bkzD2qys?si=Mt5ibd528-SuBF4R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
         </div>
         <div class="modal-footer d-flex justify-content-center">
